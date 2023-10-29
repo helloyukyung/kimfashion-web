8f9962acc9bead60f8a90ee053165fa916ea3303
Yukyung Kim / Sally <rladbrud12@naver.com>
2023-10-29 19:23:53 +0900

    feat : quote-item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main>next template</main>;
+import QuoteItem from '@/components/quote/quote-item'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <Image src={'/assets/logo.svg'} width={500} height={200} alt={'logo'} />
+      <QuoteItem />
+    </main>
+  )
 }
