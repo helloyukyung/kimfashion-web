35d6c0d3de81b69497fe77053668f54e0b42b438
Yukyung Kim / Sally <rladbrud12@naver.com>
2024-01-04 00:50:08 +0900

    feat: post detail

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -1,34 +1,50 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import {Post} from './post-list'
 
-function PostItem() {
+interface PostItemProps {
+  post: Post
+}
+function PostItem({post}: PostItemProps) {
   return (
-    <div className="w-full">
+    <Link href={`/news/${post.id}`} className="w-full">
       <div className="flex items-center gap-[10px] pb-[12px] pl-[4px] pt-[8px]">
         <Image
           priority
-          className="rounded-[100px]"
+          className="h-[32px] w-[32px] rounded-[100px]"
           width={32}
           height={32}
-          src="/assets/temp/marieclaire.png"
+          src={post.editorImage}
           alt="logo"
         />
         <div>
-          <span>마리끌레르</span>
+          <span>{post.editor}</span>
           <span className="px-[4px] text-[#737373]">·</span>
           <span className="text-[#737373]">1일</span>
         </div>
       </div>
-      <Image className="w-full rounded" width={480} height={480} src="/assets/temp/test_1.jpeg" alt="temp" />
+      <Image
+        className="max-h-[585px] w-full overflow-hidden rounded object-cover"
+        width={480}
+        height={480}
+        src={post.representativeImage}
+        alt="representative-image"
+      />
       <div className="py-[8px]">
         <h3>
-          <b className="mr-[4px]">마리끌레르</b>루이비통과 최소라가 함께한 마리끌레르 1월호 커버 공개
+          <b className="mr-[4px]">{post.editor}</b>
+          {post.title}
         </h3>
-        <div className="pt-[4px] text-[color:var(--hashtag)]">
-          <span>#Fashion</span> <span>#Trand</span>
+        <div className="pt-[8px] text-[color:var(--hashtag)]">
+          {post.hashtags.map((hashtag) => (
+            <span className="pr-[4px]" key={hashtag}>
+              #{hashtag}
+            </span>
+          ))}
         </div>
         <hr className="my-[8px]" />
       </div>
-    </div>
+    </Link>
   )
 }
 
--- src/components/post/post-list.tsx
+++ src/components/post/post-list.tsx
@@ -3,12 +3,34 @@ import PostItem from './post-item'
 interface PostListProps {
   title: string
 }
+
+const temp: Post[] = [
+  {
+    id: 'trand2024',
+    title: '2024 SS 패션 트렌드 TOP10 빠르게 알아보기',
+    representativeImage: '/assets/temp/prada.avif',
+    hashtags: ['fashion', 'trand', 'trand2024'],
+    editor: '김패션',
+    editorImage: '/assets/logo.png',
+    createdAt: '2023.12.26'
+  }
+]
+
+export interface Post {
+  id: string
+  title: string
+  representativeImage: string
+  hashtags: string[]
+  editor: string
+  editorImage: string
+  createdAt: string
+}
 function PostList({title}: PostListProps) {
   return (
     <div className="flex w-full max-w-[480px] flex-col justify-center">
       <h2 className="mb-[12px] text-[3.2rem] font-bold uppercase">{title}</h2>
-      {['', ''].map((item, index) => (
-        <PostItem key={index} />
+      {temp.map((item, index) => (
+        <PostItem post={item} key={index} />
       ))}
     </div>
   )
