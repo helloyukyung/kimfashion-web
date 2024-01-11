2cadc5247c8b62404b0ec6cd4ed185456da2316e
Yukyung Kim / Sally <rladbrud12@naver.com>
2024-01-11 22:32:21 +0900

    chore: install storybook

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
-module.exports = {
-  semi: false,
-  trailingComma: 'none',
-  singleQuote: true,
-  printWidth: 120,
-  bracketSpacing: false,
-  tabWidth: 2,
-  parser: 'typescript',
-  plugins: ['prettier-plugin-tailwindcss']
-}
+// module.exports = {
+//   semi: false,
+//   trailingComma: 'none',
+//   singleQuote: true,
+//   printWidth: 120,
+//   bracketSpacing: false,
+//   tabWidth: 2,
+//   parser: 'typescript',
+//   plugins: ['prettier-plugin-tailwindcss']
+// }
