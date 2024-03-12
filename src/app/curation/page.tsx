566f2287cc7ff67d6ebcfbbe307f6a27af1e56a0
helloyukyung <>
2024-03-12 11:06:38 +0000

    Develop (#2)
    * feat: curation form schema
    
    * feat: page data 연결
    
    * feat: HoverTooltipButton component
    
    * chore: css 수정
    
    * feat: openGraph Image &title
    
    * chore: delete mockData

--- src/app/curation/page.tsx
+++ src/app/curation/page.tsx
@@ -4,59 +4,6 @@ import Subtitle from '@/components/common/subtitle'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const mockData = [
-  {
-    title: '기본 후드티 추천',
-    representativeImage: '/assets/temp/forher.jpeg',
-    content:
-      '스웨덴을 기반으로 다양한 하위문화를 전통적인 패브릭을 통해 풀어 나가는 브랜드 아워레가시. 국내에서 큰 사랑을 받는 브랜드인 만큼 코트 또한 유명하다. 넉넉한 사이즈의 제품이지만 테일러링이 들어간 각진 어깨와 허리 라인의 실루엣이 특징. 모헤어 울의 헤어리한 텍스쳐와 만나 섹시한 무드를 자아내는 확실한 차별점을 가지고있다.',
-    hashtags: ['후드티', '기본템'],
-    curationProducts: [
-      {
-        brandName: 'forher',
-        name: 'For Her·NAPPING CROP HOODIE ZIP UP',
-        price: 10400,
-        image: '/assets/temp/forher.jpeg',
-        description: '',
-        relativeContents: []
-      },
-      {
-        brandName: 'forher',
-        name: 'For Her·NAPPING CROP HOODIE ZIP UP',
-        price: 10400,
-        image: '/assets/temp/forher.jpeg',
-        description: '',
-        relativeContents: []
-      }
-    ]
-  },
-  {
-    title: '기본 후드티 추천',
-    representativeImage: '/assets/temp/forher.jpeg',
-    content:
-      '스웨덴을 기반으로 다양한 하위문화를 전통적인 패브릭을 통해 풀어 나가는 브랜드 아워레가시. 국내에서 큰 사랑을 받는 브랜드인 만큼 코트 또한 유명하다. 넉넉한 사이즈의 제품이지만 테일러링이 들어간 각진 어깨와 허리 라인의 실루엣이 특징. 모헤어 울의 헤어리한 텍스쳐와 만나 섹시한 무드를 자아내는 확실한 차별점을 가지고있다.',
-    hashtags: ['후드티', '기본템'],
-    curationProducts: [
-      {
-        brandName: 'forher',
-        name: 'For Her·NAPPING CROP HOODIE ZIP UP',
-        price: 10400,
-        image: '/assets/temp/forher.jpeg',
-        description: '',
-        relativeContents: []
-      },
-      {
-        brandName: 'forher',
-        name: 'For Her·NAPPING CROP HOODIE ZIP UP',
-        price: 10400,
-        image: '/assets/temp/forher.jpeg',
-        description: '',
-        relativeContents: []
-      }
-    ]
-  }
-]
-
 export default async function Page() {
   const curationList = await getCurationList()
 
