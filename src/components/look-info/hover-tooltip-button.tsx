63ad1ec7f180f8153a68781303e1cf8ee338a220
helloyukyung <>
2024-03-16 17:40:13 +0000

    chore: tooltip 위치 수정

--- src/components/look-info/hover-tooltip-button.tsx
+++ src/components/look-info/hover-tooltip-button.tsx
@@ -28,34 +28,34 @@ const HoverTooltipButton = ({
     window.open(url, '_blank')
   }
 
-  const tooltipLocation =
-    x < 50
-      ? '-left-[20vw] tablet:-left-[40px] ' + (y > 50 && '-top-[120px]')
-      : '-right-[20vw] tablet:-right-[40px] ' + (y > 50 && '-top-[120px]')
+  const tooltipLocation = x < 50 ? '-left-[20vw] tablet:-left-[40px] ' : '-right-[20vw] tablet:-right-[40px] '
 
   return (
-    <div style={{left: `${x}%`, top: `${y}%`}} className="z-100 absolute">
-      <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        <button
-          type="button"
-          className="opacity- flex h-9 w-9 items-center justify-center rounded-full bg-[var(--primary-01)] opacity-85"
-        >
-          <FaPlus className="text-white" />
-        </button>
-        {hoverState && (
-          <div className={`absolute ${tooltipLocation} p-3`}>
-            <div className="flex h-[100px] w-[250px] gap-4 rounded bg-white p-3">
-              <Image className="rounded" src={image} width={80} height={80} alt="image" />
-              <div className="flex flex-1 flex-col items-start justify-between">
-                <span className="line-clamp-2">{title}</span>
-                <Button full={true} onClick={handleClickPurchase}>
-                  구매하기
-                </Button>
-              </div>
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{left: `${x}%`, top: `${y}%`}}
+      className="z-100 absolute"
+    >
+      <button
+        type="button"
+        className="opacity- flex h-9 w-9 items-center justify-center rounded-full bg-[var(--primary-01)] opacity-85"
+      >
+        <FaPlus className="text-white" />
+      </button>
+      {hoverState && (
+        <div className={`p-15 absolute ${tooltipLocation}`}>
+          <div className="flex h-[100px] w-[250px] gap-4 rounded bg-white p-3">
+            <Image className="rounded" src={image} width={80} height={80} alt="image" />
+            <div className="flex flex-1 flex-col items-start justify-between">
+              <span className="line-clamp-2">{title}</span>
+              <Button full={true} onClick={handleClickPurchase}>
+                구매하기
+              </Button>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
