fb3e1b645f3e37547d429d0b1cda60cc06de2916
helloyukyung <>
2024-03-17 17:43:43 +0000

    chore: script id 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
           <Footer />
           <ToastContainer theme="colored" position="top-center" autoClose={3000} />
         </div>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-MWEF9CKQCQ" strategy="afterInteractive" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-G8M9N52KNH" strategy="afterInteractive" />
         <Script
           id="google-analytics"
           strategy="afterInteractive"
@@ -48,7 +48,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-MWEF9CKQCQ');
+              gtag('config', 'G-G8M9N52KNH');
             `
           }}
         />
